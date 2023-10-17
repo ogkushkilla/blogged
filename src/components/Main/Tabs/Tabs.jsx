d5dbd95c7b76d1cc592f9713d62f521ad32146f3
ogkushkilla <vsevolod.kravchenko@yandex.ru>
2023-10-17 20:39:49 +0500

    практика по спискам, todo list реализовался

--- src/components/Main/Tabs/Tabs.jsx
+++ src/components/Main/Tabs/Tabs.jsx
@@ -1,21 +1,30 @@
 import style from './Tabs.module.css';
+import PropTypes from 'prop-types';
 
-export const Tabs = () => {
-  console.log();
+export const Tabs = ({list, setList, addItem}) => {
+  const handleClick = (id) => {
+    setList(list.filter(item => item.id !== id));
+  };
+
   return (
-    <ul className={style.list}>
-      <li>
-        <a href="/">Главная</a>
-      </li>
-      <li>
-        <a href="/">Просмотренные</a>
-      </li>
-      <li>
-        <a href="/">Сохраненные</a>
-      </li>
-      <li>
-        <a href="/">Мои посты</a>
-      </li>
-    </ul>
+    <>
+      <button onClick={addItem}>addItem</button>
+
+      <ul className={style.list}>
+        {list.map(({value, id}) => (
+          <li key={id}>
+            <button onClick={() => handleClick(id)}>
+              {value}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
+};
+
+Tabs.propTypes = {
+  list: PropTypes.array,
+  setList: PropTypes.func,
+  addItem: PropTypes.func,
 };
