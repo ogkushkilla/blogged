9144c5dce9e82da6230541724348ff13921b74a8
ogkushkilla <vsevolod.kravchenko@yandex.ru>
2023-10-17 22:16:04 +0500

    added stylized tabs menu + utils functions + create svg icons components

--- src/components/Main/Tabs/Tabs.jsx
+++ src/components/Main/Tabs/Tabs.jsx
@@ -1,25 +1,67 @@
+import {useEffect, useState} from 'react';
 import style from './Tabs.module.css';
 import PropTypes from 'prop-types';
+import {assignRandomId} from '../../../utils/generateRandomId';
+import {debounceRaf} from '../../../utils/debounce';
+import {ReactComponent as ArrowIcon} from './img/arrow.svg';
+import {ReactComponent as EyeIcon} from './img/eye.svg';
+import {ReactComponent as HomeIcon} from './img/home.svg';
+import {ReactComponent as PostIcon} from './img/post.svg';
+import {ReactComponent as SaveIcon} from './img/save.svg';
 
-export const Tabs = ({list, setList, addItem}) => {
-  const handleClick = (id) => {
-    setList(list.filter(item => item.id !== id));
+const LIST = [
+  {value: 'Главная', Icon: EyeIcon},
+  {value: 'Просмотренные', Icon: HomeIcon},
+  {value: 'Сохраненные', Icon: PostIcon},
+  {value: 'Мои посты', Icon: SaveIcon},
+].map(assignRandomId);
+
+export const Tabs = () => {
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  const [isDropDown, setIsDropDown] = useState(false);
+  const [selectedMenuItem, setSelectedMenuItem] = useState('Меню');
+  const handleResize = () => {
+    if (document.documentElement.clientWidth < 768) {
+      setIsDropDown(true);
+    } else {
+      setIsDropDown(false);
+    }
   };
 
-  return (
-    <>
-      <button onClick={addItem}>addItem</button>
+  useEffect(() => {
+    const debounceResize = debounceRaf(handleResize);
+    debounceResize();
+    window.addEventListener('resize', debounceResize);
+    return () => {
+      window.removeEventListener('resize', debounceResize);
+    };
+  }, []);
 
-      <ul className={style.list}>
-        {list.map(({value, id}) => (
-          <li key={id}>
-            <button onClick={() => handleClick(id)}>
+  return (
+    <div className={style.container}>
+      {isDropDown && <div className={style.wrapperBtn}>
+        <button className={style.btn}
+          onClick={() => setIsDropDownOpen(!isDropDownOpen)}>
+          {selectedMenuItem}
+          <ArrowIcon width={15} height={15} />
+        </button>
+      </div>}
+
+      {(isDropDownOpen || !isDropDown) &&
+      <ul className={style.list} onClick={() => setIsDropDownOpen(false)}>
+        {LIST.map(({value, id, Icon}) => (
+          <li className={style.item} key={id}>
+            <button className={style.btn}
+              onClick={() => {
+                setSelectedMenuItem(value);
+              }}>
               {value}
+              {Icon && <Icon width={30} height={30} />}
             </button>
           </li>
         ))}
-      </ul>
-    </>
+      </ul>}
+    </div>
   );
 };
 
