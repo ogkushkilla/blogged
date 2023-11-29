a4ec0390715bda068126f57d83ab25efa4afb896
ogkushkilla <vsevolod.kravchenko@yandex.ru>
2023-11-29 19:54:51 +0500

    added redux saga and make searching with saga

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import style from './Search.module.css';
 import {ReactComponent as SearchIcon} from './img/search.svg';
+import {useDispatch} from 'react-redux';
+import {searchRequest} from '../../../store/search/searchAction';
 
-export const Search = () => (
-  <form className={style.form}>
-    <input className={style.search} type="search" />
-    <button className={style.button}>
-      <SearchIcon className={style.svg} />
-    </button>
-  </form>
-);
+export const Search = () => {
+  const dispatch = useDispatch();
+  const [search, setSearch] = useState('');
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    dispatch(searchRequest(search));
+  };
+
+  return (
+    <form className={style.form} onSubmit={handlerSubmit}>
+      <input
+        className={style.search}
+        type="search"
+        onChange={e => setSearch(e.target.value)}
+        value={search} />
+      <button className={style.button} type='submit'>
+        <SearchIcon className={style.svg} />
+      </button>
+    </form>
+  );
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,14 @@
 import {tokenMiddleware, tokenReducer} from './tokenReducer';
 import {commentReducer} from './commentReducer';
 import {authReducer} from './auth/authReducer';
+import {searchReducer} from './search/searchReducer';
 import postsReducer from './posts/postsSlice';
 import commentsReducer from './comments/commentsSlice';
 import {configureStore} from '@reduxjs/toolkit';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
@@ -12,7 +17,10 @@ export const store = configureStore({
     authReducer,
     postsReducer,
     commentsReducer,
+    searchReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(tokenMiddleware)
+    getDefaultMiddleware().concat(tokenMiddleware, sagaMiddleware)
 });
+
+sagaMiddleware.run(rootSaga);
